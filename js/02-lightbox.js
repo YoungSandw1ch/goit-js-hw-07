973ae638cf6693f9a35bb1e70fd6840d6642a42f
Sergei Mikhailenko <sersus1991@gmail.com>
2022-02-14 15:58:26 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const itemsMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `
+}).join('');
+console.log(itemsMarkup);
